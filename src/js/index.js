20545c909efea99453ff054751879258b82d8cf5
Jakub Skoneczny <skoneczny.j@gmail.com>
2019-08-24 09:30:59 +0200

    add lazy loading and manifest:

--- src/js/index.js
+++ src/js/index.js
@@ -1,104 +1,138 @@
 import $ from "jquery";
 //window.$ = $;
 
+$(document).ready(function() {
+  // Navbar shrink on scroll
+  $(window).scroll(function() {
+    if ($(document).scrollTop() > 50) {
+      $(".header__nav").addClass("js-shrink");
+    } else {
+      $(".header__nav").removeClass("js-shrink");
+    }
+  });
 
-$(document).ready(function () {
-	// Navbar shrink on scroll
-	$(window).scroll(function () {
-		if ($(document).scrollTop() > 50) {
-			$(".header__nav").addClass("js-shrink");
-		} else {
-			$(".header__nav").removeClass("js-shrink");
-		}
-	});
+  $(function() {
+    $("a[href*=#]:not([href=#])").click(function() {
+      if (
+        location.pathname.replace(/^\//, "") ==
+          this.pathname.replace(/^\//, "") &&
+        location.hostname == this.hostname
+      ) {
+        var target = $(this.hash);
+        target = target.length
+          ? target
+          : $("[name=" + this.hash.slice(1) + "]");
+        if (target.length) {
+          $("html,body").animate(
+            {
+              scrollTop: target.offset().top + 70
+            },
+            1200
+          );
+          return false;
+        }
+      }
+    });
+  });
 
-	$(function () {
-		$('a[href*=#]:not([href=#])').click(function () {
-			if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+  var sections = $("section"),
+    nav = $("nav"),
+    nav_height = nav.outerHeight();
 
-				var target = $(this.hash);
-				target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-				if (target.length) {
-					$('html,body').animate({
-						scrollTop: target.offset().top + 70
-					}, 1200);
-					return false;
-				}
-			}
-		});
-	});
+  $(window).load(checkState);
+  $(window).on("scroll", checkState);
 
-	var sections = $('section')
-		, nav = $('nav')
-		, nav_height = nav.outerHeight();
+  checkState();
 
-	$(window).load(checkState);
-	$(window).on('scroll', checkState);
+  function checkState() {
+    var cur_pos = $(this).scrollTop();
 
-	checkState();
+    sections.each(function() {
+      var top = $(this).offset().top - nav_height,
+        bottom = top + $(this).outerHeight();
 
-	function checkState() {
-		var cur_pos = $(this).scrollTop();
+      if (cur_pos >= top && cur_pos <= bottom) {
+        nav.find("a").removeClass("header__link--active");
+        //sections.removeClass('header__link--active');
 
-		sections.each(function () {
-			var top = $(this).offset().top - nav_height,
-				bottom = top + $(this).outerHeight();
+        //$(this).addClass('header__link--active');
+        nav
+          .find('a[href="#' + $(this).attr("id") + '"]')
+          .addClass("header__link--active");
+      }
+    });
+  }
 
-			if (cur_pos >= top && cur_pos <= bottom) {
-				nav.find('a').removeClass('header__link--active');
-				//sections.removeClass('header__link--active');
+  function hasTouch() {
+    return (
+      "ontouchstart" in document.documentElement ||
+      navigator.maxTouchPoints > 0 ||
+      navigator.msMaxTouchPoints > 0
+    );
+  }
 
-				//$(this).addClass('header__link--active');
-				nav.find('a[href="#' + $(this).attr('id') + '"]').addClass('header__link--active');
-			}
-		});
-	}
+  if (hasTouch()) {
+    // remove all :hover stylesheets
+    try {
+      // prevent exception on browsers not supporting DOM styleSheets properly
+      for (var si in document.styleSheets) {
+        var styleSheet = document.styleSheets[si];
+        if (!styleSheet.rules) continue;
 
-	function hasTouch() {
-		return 'ontouchstart' in document.documentElement
-			|| navigator.maxTouchPoints > 0
-			|| navigator.msMaxTouchPoints > 0;
-	}
+        for (var ri = styleSheet.rules.length - 1; ri >= 0; ri--) {
+          if (!styleSheet.rules[ri].selectorText) continue;
 
-	if (hasTouch()) { // remove all :hover stylesheets
-		try { // prevent exception on browsers not supporting DOM styleSheets properly
-			for (var si in document.styleSheets) {
-				var styleSheet = document.styleSheets[si];
-				if (!styleSheet.rules) continue;
+          if (styleSheet.rules[ri].selectorText.match(":hover")) {
+            styleSheet.deleteRule(ri);
+          }
+        }
+      }
+    } catch (ex) {}
+  }
 
-				for (var ri = styleSheet.rules.length - 1; ri >= 0; ri--) {
-					if (!styleSheet.rules[ri].selectorText) continue;
+  function hoverTouchUnstick() {
+    // Check if the device supports touch events
+    if ("ontouchstart" in document.documentElement) {
+      // Loop through each stylesheet
+      for (
+        var sheetI = document.styleSheets.length - 1;
+        sheetI >= 0;
+        sheetI--
+      ) {
+        var sheet = document.styleSheets[sheetI];
+        // Verify if cssRules exists in sheet
+        if (sheet.cssRules) {
+          // Loop through each rule in sheet
+          for (var ruleI = sheet.cssRules.length - 1; ruleI >= 0; ruleI--) {
+            var rule = sheet.cssRules[ruleI];
+            // Verify rule has selector text
+            if (rule.selectorText) {
+              // Replace hover psuedo-class with active psuedo-class
+              rule.selectorText = rule.selectorText.replace(
+                ":hover",
+                ":active"
+              );
+            }
+          }
+        }
+      }
+    }
+  }
 
-					if (styleSheet.rules[ri].selectorText.match(':hover')) {
-						styleSheet.deleteRule(ri);
-					}
-				}
-			}
-		} catch (ex) { }
-	}
+  hoverTouchUnstick();
 
-	function hoverTouchUnstick() {
-		// Check if the device supports touch events
-		if('ontouchstart' in document.documentElement) {
-			// Loop through each stylesheet
-			for(var sheetI = document.styleSheets.length - 1; sheetI >= 0; sheetI--) {
-				var sheet = document.styleSheets[sheetI];
-				// Verify if cssRules exists in sheet
-				if(sheet.cssRules) {
-					// Loop through each rule in sheet
-					for(var ruleI = sheet.cssRules.length - 1; ruleI >= 0; ruleI--) {
-						var rule = sheet.cssRules[ruleI];
-						// Verify rule has selector text
-						if(rule.selectorText) {
-							// Replace hover psuedo-class with active psuedo-class
-							rule.selectorText = rule.selectorText.replace(":hover", ":active");
-						}
-					}
-				}
-			}
-		}
-	}
-
-	hoverTouchUnstick();
-
+  // Lazyloading
+  if ("loading" in HTMLImageElement.prototype) {
+    const images = document.querySelectorAll("img.lazyload");
+    images.forEach(img => {
+      img.src = img.dataset.src;
+    });
+  } else {
+    // Dynamically import the LazySizes library
+    let script = document.createElement("script");
+    script.async = true;
+    script.src =
+      "https://cdnjs.cloudflare.com/ajax/libs/lazysizes/4.1.8/lazysizes.min.js";
+    document.body.appendChild(script);
+  }
 });
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,13 @@
 const path = require("path");
-const HtmlWebPackPlugin = require('html-webpack-plugin');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
 const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const ImageminPlugin = require('imagemin-webpack-plugin').default;
+const UglifyJSPlugin = require("uglifyjs-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const ImageminPlugin = require("imagemin-webpack-plugin").default;
 
 module.exports = {
-  entry: ["babel-polyfill", "./src/js/index.js", "./src/sass/index.scss" ],
+  entry: ["babel-polyfill", "./src/js/index.js", "./src/sass/index.scss"],
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "assets/bundle.js"
@@ -26,58 +26,65 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        use: [ MiniCssExtractPlugin.loader, "css-loader", 'postcss-loader' ]
+        use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader"]
       },
       {
         test: /\.scss$/,
-        use: [ MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader' ]
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "postcss-loader",
+          "sass-loader"
+        ]
       },
       {
         test: /\.(png|jpe?g|gif)$/,
         use: [
           {
-            loader: 'url-loader',
+            loader: "url-loader",
             options: {
               limit: 8192,
               name: "[name].[ext]",
               outputPath: "./assets/img",
               publicPath: "./img"
-            },
-          },
+            }
+          }
         ]
       },
       {
         test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
         use: [
           {
-            loader: 'url-loader',
+            loader: "url-loader",
             options: {
               limit: 28192,
               name: "[name].[ext]",
               outputPath: "./assets/fonts",
               publicPath: "./fonts"
-            },
-          }]
+            }
+          }
+        ]
       }
     ]
   },
   plugins: [
     new HtmlWebPackPlugin({
       template: "./src/index.html",
-      filename: "./index.html",
+      filename: "./index.html"
     }),
     new MiniCssExtractPlugin({
       filename: "./assets/style.css"
     }),
     new CopyWebpackPlugin([
-      {from:'src/img',to:'./assets/img'} 
+      { from: "src/manifest.json", to: "./assets/manifest.json" },
+      { from: "src/img", to: "./assets/img" }
     ]),
     new ImageminPlugin({
       test: /\.(jpe?g|png|gif|svg)$/i,
       pngquant: {
-        quality: '30-65'
+        quality: "30-65"
       }
-    }) 
+    })
   ],
   optimization: {
     minimizer: [
@@ -90,10 +97,10 @@ module.exports = {
           collapseWhitespace: true,
           preserveLineBreaks: false
         }
-      }),
+      })
     ]
   },
   devServer: {
     contentBase: "./dist"
-  },
+  }
 };
