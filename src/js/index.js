6a5d73c004b405b16ee3b288598c604c3df5ec89
Jakub Skoneczny <skoneczny.j@gmail.com>
2019-08-24 10:16:35 +0200

    disable error script in hover states

--- src/js/index.js
+++ src/js/index.js
@@ -90,37 +90,6 @@ $(document).ready(function() {
     } catch (ex) {}
   }
 
-  function hoverTouchUnstick() {
-    // Check if the device supports touch events
-    if ("ontouchstart" in document.documentElement) {
-      // Loop through each stylesheet
-      for (
-        var sheetI = document.styleSheets.length - 1;
-        sheetI >= 0;
-        sheetI--
-      ) {
-        var sheet = document.styleSheets[sheetI];
-        // Verify if cssRules exists in sheet
-        if (sheet.cssRules) {
-          // Loop through each rule in sheet
-          for (var ruleI = sheet.cssRules.length - 1; ruleI >= 0; ruleI--) {
-            var rule = sheet.cssRules[ruleI];
-            // Verify rule has selector text
-            if (rule.selectorText) {
-              // Replace hover psuedo-class with active psuedo-class
-              rule.selectorText = rule.selectorText.replace(
-                ":hover",
-                ":active"
-              );
-            }
-          }
-        }
-      }
-    }
-  }
-
-  hoverTouchUnstick();
-
   // Lazyloading
   if ("loading" in HTMLImageElement.prototype) {
     const images = document.querySelectorAll("img.lazyload");
